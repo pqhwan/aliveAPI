db25a76c9db8f0c4264915112340d7d3eb88fcb9
pqhwan <hyun_sik_kim@brown.edu>
2015-02-12 11:55:27 -0500

    Initial Commit

---
+++ aliveApi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  aliveApi
+//
+//  Created by Pete Kim on 2/12/15.
+//  Copyright (c) 2015 Pete Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
